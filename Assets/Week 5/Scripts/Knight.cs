7a784762a2997145be1a80d41118d24a5b93b0d4
Lorraine Slakinski <slakinsa@shernet.sheridancollege.ca>
2024-02-08 15:17:56 -0500

    Task2 complete

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -8,6 +8,8 @@ public class Knight : MonoBehaviour
     Vector2 destination;
     Vector2 movement;
     public float speed = 3;
+    public float maxHealth = 5;
+    float health;
     Rigidbody2D rb;
     Animator animator;
     bool clickingOnSelf = false;
@@ -16,7 +18,7 @@ public class Knight : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = rb.GetComponent<Animator>();
-
+        health = maxHealth;
     }
 
     private void FixedUpdate()
@@ -44,11 +46,26 @@ public class Knight : MonoBehaviour
     private void OnMouseDown()
     {
         clickingOnSelf = true;
-        animator.SetTrigger("TakeDamage");
+        SendMessage("TakeDamage", 1);
     }
 
     private void OnMouseUp()
     {
         clickingOnSelf = false;
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if (health == 0)
+        {
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+        }
     }
 }
