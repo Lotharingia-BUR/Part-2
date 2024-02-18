44372412bbd78e648aa78af2da46a00cb9aef94e
Lorraine <lorraine@sdf1.net>
2024-02-18 11:53:31 -0500

    Start of code + background

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //initiate components
+    Rigidbody2D rb;
+    Animator am;
+
+    //public variables
+    public float speed = 1;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
