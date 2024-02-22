234871b3f53e02e88b3018a9e807994de30e0c9a
Lorraine Slakinski <slakinsa@shernet.sheridancollege.ca>
2024-02-22 14:08:35 -0500

    Highlight colour

--- Assets/Week 7/Scripts/Teammate.cs
+++ Assets/Week 7/Scripts/Teammate.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class Teammate : MonoBehaviour
 {
-    public void Selected()
-    {
 
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Selected(false);
+        }*/
+    }
+            public void Selected( bool isSelected)
+    {
+        if (isSelected)
+        {
+            sr.color = Color.yellow;
+        }
+        else if (!isSelected)
+        {
+            sr.color = Color.red;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
     }
 }
